bff9c03cbd8b13239b74ae2954f51700fba0ab96
it19951386 <it19951386@my.sliit.lk>
2021-09-18 18:59:57 +0530

    Configure navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:boc_mobile_app/screens/pasindu/customAppBar.dart';
 import 'package:boc_mobile_app/screens/pasindu/customBottomNavigationBar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:boc_mobile_app/screens/pasindu/test.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -31,14 +34,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List _children = [Test1(), Test2(), Test3()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(appBarTitle: 'Dashboard'),
-      body: Center(
-        child: Text("Hello World"),
+      body: _children[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 253, 197, 12),
+              Color.fromARGB(255, 255, 231, 107)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+            stops: [0.0, 0.8],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: onTabTapped,
+          showUnselectedLabels: true,
+          backgroundColor: Colors.transparent,
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.white,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.landmark),
+              label: 'Dashboard',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarms),
+              label: 'Reminders',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.bars),
+              label: 'More',
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar:bottomNavBar(),
     );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
---
+++ lib/screens/pasindu/test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Test1 extends StatelessWidget {
+  const Test1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("1"),
+      ),
+    );
+  }
+}
+
+class Test2 extends StatelessWidget {
+  const Test2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("2"),
+      ),
+    );
+  }
+}
+
+class Test3 extends StatelessWidget {
+  const Test3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("3"),
+      ),
+    );
+  }
+}
