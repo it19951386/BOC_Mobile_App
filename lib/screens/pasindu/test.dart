d70b83ef94ea08500f1c510967f57f409d3f70e3
it19951386 <it19951386@my.sliit.lk>
2021-09-18 20:59:59 +0530

    Create two card components

--- lib/screens/pasindu/test.dart
+++ lib/screens/pasindu/test.dart
@@ -5,15 +5,62 @@ class Test1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(4, (index) {
-          return Center(
-            child: Text('$index'),
-          );
-        }),
-      ),
+    return Column(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 120,
+              width: 366,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 120,
+              width: 366,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [Text("d"), Text("d")],
+        ),
+        Row(
+          children: [Text("d"), Text("d")],
+        ),
+      ],
     );
   }
 }
