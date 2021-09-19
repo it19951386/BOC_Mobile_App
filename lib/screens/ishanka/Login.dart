bf8a5566e1e175899ac32f7cdfab2a786d7257b9
D.I.P Lakshan <it19951836@my.sliit,lk>
2021-09-19 13:45:29 +0530

    Add background image to login page

---
+++ lib/screens/ishanka/Login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'widgets/widgets.dart';
+
+void main() => runApp(MaterialApp(
+  home: Login(),
+  debugShowCheckedModeBanner: false,
+));
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundImage();
+  }
+}
+
+
