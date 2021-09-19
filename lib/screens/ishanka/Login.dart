36544111ddf3969771deb3d552220c86fb1ab612
D.I.P Lakshan <it19951836@my.sliit,lk>
2021-09-19 15:53:00 +0530

    add safe Area widget

--- lib/screens/ishanka/Login.dart
+++ lib/screens/ishanka/Login.dart
@@ -1,5 +1,6 @@
-import 'package:boc_mobile_app/screens/ishanka/palatte.dart';
+import 'palatte.dart';
 import 'package:flutter/material.dart';
+import 'widgets/clickable-text.dart';
 import 'widgets/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -19,119 +20,123 @@ class Login extends StatelessWidget {
         Scaffold(
           backgroundColor: Colors.transparent,
 
-          body: Column(
-            children: [
-              Container(
-                height: 250,
-                child: Center(
-                  child: Image(
-                      image:AssetImage('assets/logo.png')
-                  ),
-                ),
-              ),
-
-              Padding(
-                padding:EdgeInsets.fromLTRB(10.0,0.0, 275.0, 0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Sign In',
-                      style:SignInText,
-                    ),
-
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding:EdgeInsets.fromLTRB(10.0,0.0, 170.0, 0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Please sign in to continue.',
-                      style:NText
-                    ),
-
-                  ],
-                ),
-              ),
-
-              SizedBox(
-                height: 30,
-              ),
-
-              //text filed
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                ),
-                child: Column(
-                  children: [
-                    TextInput(
-                      icon: FontAwesomeIcons.user,
-                      hint: 'Email',
-                      inputType: TextInputType.emailAddress,
-                      inputAction: TextInputAction.next,
-                    ),
-
-                    SizedBox(
-                      height: 18,
-                    ),
-
-                    PasswordInput(
-                      icon: FontAwesomeIcons.lock,
-                      hint: 'Password',
-                      inputAction: TextInputAction.next,
-                    ),
-                  ],
-                ),
-              ),
-
-
-              SizedBox(
-                height: 18,
-              ),
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
+          body: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
                 children: [
-                  //1
-                  RoundedButton(
-                    buttonText: 'Sign In',
-                  ),
-
-                  //2
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 2,vertical: 20),
-                    child: Text(
-                      "Forget Password?",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black54
+                  Container(
+                    height: 250,
+                    child: Center(
+                      child: Image(
+                          image:AssetImage('assets/logo.png')
                       ),
                     ),
                   ),
 
-                  //3
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:55,vertical:20),
-                    child: ClickableText(),
+                    padding:EdgeInsets.fromLTRB(10.0,0.0, 275.0, 0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Sign In',
+                          style:SignInText,
+                        ),
+
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding:EdgeInsets.fromLTRB(10.0,0.0, 170.0, 0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Please sign in to continue.',
+                          style:NText
+                        ),
+
+                      ],
+                    ),
                   ),
 
-                  //4
-                  FloatingActionButton(
-                    onPressed: (){},
-                    backgroundColor: Colors.amberAccent,
-                    child: Icon(
-                      FontAwesomeIcons.phoneAlt,
-                      color: Colors.black,
+                  SizedBox(
+                    height: 30,
+                  ),
+
+                  //text filed
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
                     ),
-                  )
+                    child: Column(
+                      children: [
+                        TextInput(
+                          icon: FontAwesomeIcons.user,
+                          hint: 'Email',
+                          inputType: TextInputType.emailAddress,
+                          inputAction: TextInputAction.next,
+                        ),
+
+                        SizedBox(
+                          height: 18,
+                        ),
+
+                        PasswordInput(
+                          icon: FontAwesomeIcons.lock,
+                          hint: 'Password',
+                          inputAction: TextInputAction.next,
+                        ),
+                      ],
+                    ),
+                  ),
+
+
+                  SizedBox(
+                    height: 18,
+                  ),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      //1
+                      RoundedButton(
+                        buttonText: 'Sign In',
+                      ),
+
+                      //2
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2,vertical: 20),
+                        child: Text(
+                          "Forget Password?",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black54
+                          ),
+                        ),
+                      ),
+
+                      //3
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal:55,vertical:20),
+                        child: ClickableText(),
+                      ),
+
+                      //4
+                      FloatingActionButton(
+                        onPressed: (){},
+                        backgroundColor: Colors.amberAccent,
+                        child: Icon(
+                          FontAwesomeIcons.phoneAlt,
+                          color: Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ],
