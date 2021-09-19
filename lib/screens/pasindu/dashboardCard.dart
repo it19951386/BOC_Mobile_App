5ecc4cb0e4fd43ed0ec785332c3d1b23f71ecf16
it19951386 <it19951386@my.sliit.lk>
2021-09-19 18:49:19 +0530

    add navigation to card view tap

--- lib/screens/pasindu/dashboardCard.dart
+++ lib/screens/pasindu/dashboardCard.dart
@@ -1,3 +1,4 @@
+import 'package:boc_mobile_app/screens/pasindu/LoanList.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,39 +14,48 @@ class dashboardCard extends StatelessWidget {
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 12),
-      height: 120,
-      width: 366,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25),
-            spreadRadius: 1,
-            blurRadius: 7,
-            offset: Offset(0, 4), // changes position of shadow
+    return InkWell(
+        onTap: () {
+          print("card tapped");
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => LoanList()),
+          );
+        },
+        child: Container(
+          margin: EdgeInsets.only(top: 12),
+          height: 120,
+          width: 366,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.25),
+                spreadRadius: 1,
+                blurRadius: 7,
+                offset: Offset(0, 4), // changes position of shadow
+              ),
+            ],
           ),
-        ],
-      ),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Container(
-          margin: EdgeInsets.only(left: 12),
-          child: Text(
-            this.title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(right: 12),
-          child: Text(this.amount,
-              style: TextStyle(
-                  color: this.color,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18)),
-        )
-      ]),
-    );
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Container(
+              margin: EdgeInsets.only(left: 12),
+              child: Text(
+                this.title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 12),
+              child: Text(this.amount,
+                  style: TextStyle(
+                      color: this.color,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18)),
+            )
+          ]),
+        ));
   }
 }
