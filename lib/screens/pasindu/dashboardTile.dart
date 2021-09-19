e84386ab1843e0d580ccd9da8c8f397b793457a5
it19951386 <it19951386@my.sliit.lk>
2021-09-19 18:36:40 +0530

    add InkWell widget to tile widget

--- lib/screens/pasindu/dashboardTile.dart
+++ lib/screens/pasindu/dashboardTile.dart
@@ -8,25 +8,30 @@ class dashboardTile extends StatelessWidget {
   final String title;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 12),
-      height: 120,
-      width: 162,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25),
-            spreadRadius: 1,
-            blurRadius: 7,
-            offset: Offset(0, 4), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [this.icon, Text(this.title)],
+    return InkWell(
+      onTap: () {
+        print("tapped");
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 12),
+        height: 120,
+        width: 162,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              spreadRadius: 1,
+              blurRadius: 7,
+              offset: Offset(0, 4), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [this.icon, Text(this.title)],
+        ),
       ),
     );
   }
