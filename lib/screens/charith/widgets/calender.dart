494f69acda228bbfbb4ae81124eb96aa9f1eb460
IT19986722-Charith <it19986722@my.sliit.lk>
2021-09-24 20:22:39 +0530

    import utill.dart for calender widget

--- lib/screens/charith/widgets/calender.dart
+++ lib/screens/charith/widgets/calender.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:boc_mobile_app/screens/charith/data/utils.dart';
 
 class Calender extends StatefulWidget {
   const Calender({Key? key}) : super(key: key);
