41063bf6153bc02a36cc53131107568f84e135d2
IT19986722-Charith <it19986722@my.sliit.lk>
2021-09-24 20:35:24 +0530

    add format to TableCalendar

--- lib/screens/charith/widgets/calender.dart
+++ lib/screens/charith/widgets/calender.dart
@@ -24,6 +24,43 @@ class _CalenderState extends State<Calender> {
         margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TableCalendar(
+              firstDay: kFirstDay,
+              lastDay: kLastDay,
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                // Use `selectedDayPredicate` to determine which day is currently selected.
+                // If this returns true, then `day` will be marked as selected.
+
+                // Using `isSameDay` is recommended to disregard
+                // the time-part of compared DateTime objects.
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  // Call `setState()` when updating the selected day
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  // Call `setState()` when updating calendar format
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                // No need to call `setState()` here
+                _focusedDay = focusedDay;
+              },
+            ),
+          ],
         ),
       ),
     );
