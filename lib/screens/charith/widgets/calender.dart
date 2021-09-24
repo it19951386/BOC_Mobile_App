52492c25665f9e7baa7117941f709d3d05b29518
IT19986722-Charith <it19986722@my.sliit.lk>
2021-09-24 20:21:16 +0530

    import calender dependancies

--- lib/screens/charith/widgets/calender.dart
+++ lib/screens/charith/widgets/calender.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calender extends StatefulWidget {
   const Calender({Key? key}) : super(key: key);
