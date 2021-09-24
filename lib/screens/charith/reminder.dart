c298380a98c00bae488176ad7281a87c388d20fe
IT19986722-Charith <it19986722@my.sliit.lk>
2021-09-24 20:40:08 +0530

    importing tablecalender packegein reminder.dart

--- lib/screens/charith/reminder.dart
+++ lib/screens/charith/reminder.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:boc_mobile_app/screens/charith/widgets/calender.dart';
 
 class Reminder extends StatefulWidget {
   const Reminder({Key? key}) : super(key: key);
