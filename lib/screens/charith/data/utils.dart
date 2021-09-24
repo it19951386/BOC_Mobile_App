12f643bac05fbf834804b527a85ea2bfa1be8c59
IT19986722-Charith <it19986722@my.sliit.lk>
2021-09-24 20:16:58 +0530

    add calender events

--- lib/screens/charith/data/utils.dart
+++ lib/screens/charith/data/utils.dart
@@ -1,1 +1,13 @@
 import 'dart:collection';
+
+import 'package:table_calendar/table_calendar.dart';
+
+/// Example event class.
+class Event {
+  final String title;
+
+  const Event(this.title);
+
+  @override
+  String toString() => title;
+}
