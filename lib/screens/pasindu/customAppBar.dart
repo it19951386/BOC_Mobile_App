19f9460be3e39bfbf55fac096dfadc1ef76fa45b
it19951386 <it19951386@my.sliit.lk>
2021-09-25 13:42:58 +0530

    Change widget name

--- lib/screens/pasindu/customAppBar.dart
+++ lib/screens/pasindu/customAppBar.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 
-class customAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String appBarTitle;
   final double barHeight = 55;
-  const customAppBar({Key? key, required this.appBarTitle}) : super(key: key);
+  const CustomAppBar({Key? key, required this.appBarTitle}) : super(key: key);
 
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
   @override
   Widget build(BuildContext context) {
-    final double statusBarHeight = MediaQuery
-        .of(context)
-        .padding
-        .top;
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
     // return AppBar(
     //   backgroundColor: Color.fromARGB(255, 253, 197, 12),
     //   title: Text(widget.appBarTitle),
@@ -19,29 +16,33 @@ class customAppBar extends StatelessWidget implements PreferredSizeWidget {
     return new Container(
       padding: EdgeInsets.only(top: statusBarHeight),
       height: statusBarHeight + barHeight,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: 
-                Text(
-                appBarTitle,
-                style: TextStyle(fontSize: 22.0, color: Colors.black, fontWeight: FontWeight.bold),
-              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Text(
+              appBarTitle,
+              style: TextStyle(
+                  fontSize: 22.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
             ),
-        ],),
+          ),
+        ],
+      ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: [Color.fromARGB(255, 253, 197, 12), Color.fromARGB(255,255,231,107)],
+            colors: [
+              Color.fromARGB(255, 253, 197, 12),
+              Color.fromARGB(255, 255, 231, 107)
+            ],
             begin: const FractionalOffset(0.0, 0.0),
             end: const FractionalOffset(0.9, 0.0),
             stops: [0.0, 1.0],
-            tileMode: TileMode.clamp
-        ),
+            tileMode: TileMode.clamp),
       ),
     );
   }
-  }
-
+}
