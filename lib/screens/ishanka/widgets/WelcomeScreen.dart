7280507389de1c4df7bcc15fb3ec1556a9f4d6a1
D.I.P Lakshan <it19951836@my.sliit.lk>
2021-09-25 13:07:17 +0530

    Create stateless widget for WelcomeScreen

---
+++ lib/screens/ishanka/widgets/WelcomeScreen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+void main() =>runApp(MaterialApp(
+  home: WelcomeScreen(),
+  debugShowCheckedModeBanner: false,
+));
+
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
