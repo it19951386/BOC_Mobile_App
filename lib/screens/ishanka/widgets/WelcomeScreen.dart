ec16487d5e85f302cc7cfb0fc4db7856713b2b64
D.I.P Lakshan <it19951836@my.sliit.lk>
2021-09-25 13:15:13 +0530

    add logo to WelcomeScreen

--- lib/screens/ishanka/widgets/WelcomeScreen.dart
+++ lib/screens/ishanka/widgets/WelcomeScreen.dart
@@ -15,6 +15,22 @@ class WelcomeScreen extends StatelessWidget {
     return Stack(
       children: [
         BackgroundImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+
+          body: Column(
+            children: [
+             Container(
+               child: Center(
+                 child: Image(
+                     image:AssetImage('assets/logo.png')
+                 ),
+               ),
+               padding: EdgeInsets.fromLTRB(0, 90, 0, 50),
+             ),
+            ],
+          ),
+        ),
       ],
     );
   }
