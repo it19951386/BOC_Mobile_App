98bdf1716f91ad0faa4541114543dbad82edbc9a
it19951386 <it19951386@my.sliit.lk>
2021-09-25 13:44:28 +0530

    add correct margin to widgets

--- lib/screens/pasindu/dashboardCard.dart
+++ lib/screens/pasindu/dashboardCard.dart
@@ -16,48 +16,51 @@ class dashboardCard extends StatelessWidget {
   final Widget nextScreen;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-        onTap: () {
-          print("card tapped");
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => this.nextScreen),
-          );
-        },
-        child: Container(
-          margin: EdgeInsets.only(top: 12),
-          height: 120,
-          width: 366,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.25),
-                spreadRadius: 1,
-                blurRadius: 7,
-                offset: Offset(0, 4), // changes position of shadow
-              ),
-            ],
-          ),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Container(
-              margin: EdgeInsets.only(left: 12),
-              child: Text(
-                this.title,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
+    return Expanded(
+      flex: 1,
+      child: InkWell(
+          onTap: () {
+            print("card tapped");
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => this.nextScreen),
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.only(left: 23, right: 23),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.25),
+                  spreadRadius: 1,
+                  blurRadius: 7,
+                  offset: Offset(0, 4), // changes position of shadow
+                ),
+              ],
             ),
-            Container(
-              margin: EdgeInsets.only(right: 12),
-              child: Text(this.amount,
-                  style: TextStyle(
-                      color: this.color,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18)),
-            )
-          ]),
-        ));
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 12),
+                    child: Text(
+                      this.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 12),
+                    child: Text(this.amount,
+                        style: TextStyle(
+                            color: this.color,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18)),
+                  )
+                ]),
+          )),
+    );
   }
 }
