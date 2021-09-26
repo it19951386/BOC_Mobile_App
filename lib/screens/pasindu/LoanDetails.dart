7e48ab94a18fbcc5f8f60f010862442358af992f
it19951386 <it19951386@my.sliit.lk>
2021-09-26 18:54:59 +0530

    add texts and card view

--- lib/screens/pasindu/LoanDetails.dart
+++ lib/screens/pasindu/LoanDetails.dart
@@ -29,6 +29,7 @@ class _LoanDetailsState extends State<LoanDetails> {
               ),
             ),
           ),
+          LoanBalanceCard(),
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
@@ -60,7 +61,7 @@ class _LoanDetailsState extends State<LoanDetails> {
                 child: Column(
                   children: [
                     Padding(
-                      padding: EdgeInsets.only(top: 12),
+                      padding: EdgeInsets.only(top: 15),
                       child: Container(
                         decoration: BoxDecoration(
                           color: Color.fromARGB(255, 253, 197, 12),
@@ -98,14 +99,132 @@ class _LoanDetailsState extends State<LoanDetails> {
                           isSelected: isSelected,
                         ),
                       ),
-                    )
+                    ),
+                    Body(),
                   ],
                 ),
               ),
             ],
-          )
+          ),
         ],
       ),
+    );
+  }
+}
+
+class LoanBalanceCard extends StatelessWidget {
+  const LoanBalanceCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData = MediaQuery.of(context);
+    return Padding(
+      padding: const EdgeInsets.only(top: 33),
+      child: Container(
+        height: 130,
+        width: queryData.size.width * 0.9,
+        margin: EdgeInsets.only(left: 23, right: 23),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              spreadRadius: 1,
+              blurRadius: 7,
+              offset: Offset(0, 4), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 12),
+              child: Text(
+                'Balance',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 12),
+              child: Text(
+                'LKR 23,000.00 ',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 64),
+        Text('Account Details0',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+        Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Text('Áccount',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text('085227453', style: TextStyle(fontSize: 18)),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Text('Customer ID',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text('0221547835', style: TextStyle(fontSize: 18)),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Text('Product Name',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child:
+                    Text('ABC Personal Loan', style: TextStyle(fontSize: 18)),
+              )
+            ],
+          ),
+        ])
+      ],
     );
   }
 }
