198bfb22217a0c1f794c4c0bee8c6a92b8913b94
D.I.P Lakshan <it19951836@my.sliit.lk>
2021-09-27 13:22:53 +0530

    Create SignUp.dart

---
+++ lib/screens/ishanka/SignUp.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'widgets/widgets.dart';
+import 'palatte.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BackgroundImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+
+
+        ),
+      ],
+    );
+  }
+}
