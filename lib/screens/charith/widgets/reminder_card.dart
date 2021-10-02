483acc428fd4a0cdc8c60eda514d4aff1d56498f
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:12:18 +0530

    create reminder card widget

---
+++ lib/screens/charith/widgets/reminder_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:boc_mobile_app/screens/charith/constants/app_textStyle.dart';
+
+class ReminderCard extends StatelessWidget {
+  const ReminderCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
