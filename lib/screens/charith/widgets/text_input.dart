a0c615170cae0332b5d19cbbdf67a2b3965fe4bf
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-02 23:55:23 +0530

    add constant to textInput

--- lib/screens/charith/widgets/text_input.dart
+++ lib/screens/charith/widgets/text_input.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
 
 class TextInput extends StatelessWidget {
-  const TextInput({Key? key}) : super(key: key);
+  const TextInput({
+    Key? key,
+    required this.icon,
+    required this.hint,
+    required this.label,
+    required this.inputType,
+    required this.inputAction,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String hint;
+  final String label;
+  final TextInputType inputType;
+  final TextInputAction inputAction;
 
   @override
   Widget build(BuildContext context) {
