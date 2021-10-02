5f784b9daeb5187008d4163d86af6b6fd513c2fb
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-02 23:58:57 +0530

    create description TextField

---
+++ lib/screens/charith/widgets/descriptionText.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
+
+class DescriptionText extends StatelessWidget {
+  const DescriptionText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
