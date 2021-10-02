f99311cccde2844c85c0c8bf2c091e131f4664ed
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 01:33:40 +0530

    add comments to export-widget

--- lib/screens/charith/widgets/export_widegts.dart
+++ lib/screens/charith/widgets/export_widegts.dart
@@ -1,14 +1,32 @@
+//Calender widget
 export 'calender.dart';
+
+// Reminder card Widget
+export 'reminder_card.dart';
+
+// Customize colors and Text widget
 export '../constants/app_textStyle.dart';
 export '../constants/color_constant.dart';
-export 'Save_Button.dart';
-export 'Clear_Button.dart';
+
+// InputField widgets
 export 'text_input.dart';
 export 'descriptionText.dart';
 export 'datePicker.dart';
-export 'reminder_card.dart';
-export 'activityCard.dart';
+
+// Buttons
+export 'Clear_Button.dart';
+export 'Save_Button.dart';
+
+//Account Card Widget
+export 'accountCards.dart';
+
+// Account Activity tab Widgets
 export 'accountTab_View.dart';
+export 'activityCard.dart';
+
+// Account Details tab Widgets
 export 'detailsTab.dart';
 export 'detailsCard.dart';
-export 'accountCards.dart';
+
+//add Account
+export '../addAccount.dart';
