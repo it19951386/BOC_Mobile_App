5261c22af4565c651429a69a0ce609c3cc4f381c
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:01:38 +0530

    add save button content and styles

--- lib/screens/charith/widgets/Save_Button.dart
+++ lib/screens/charith/widgets/Save_Button.dart
@@ -5,6 +5,37 @@ class SaveButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.amber,
+              border: Border.all(width: 2.0, color: Colors.black12),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 5, color: Colors.grey, offset: Offset(0, 4))
+              ]),
+          child: FlatButton(
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                child: Text(
+                  'Save',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins-Medium',
+                  ),
+                ),
+              ),
+              color: Colors.amber,
+              shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(20.0),
+              )),
+        ),
+      ],
+    );
   }
 }
