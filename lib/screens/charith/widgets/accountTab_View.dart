963876b9188bdee609a64af554f4efe65b71fa89
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:59:48 +0530

    add account Tab content

--- lib/screens/charith/widgets/accountTab_View.dart
+++ lib/screens/charith/widgets/accountTab_View.dart
@@ -8,7 +8,32 @@ class ActivityTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        children: [],
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ActivityCard(
+                  date: '09/05/2021',
+                  activity: 'TRANSFER CREDIT TO 9578642XXX',
+                ),
+                ActivityCard(
+                  date: '02/05/2021',
+                  activity: 'TRANSFER CREDIT TO 8474142XXX',
+                ),
+                ActivityCard(
+                  date: '26/04/2021',
+                  activity: 'PAY Utility Bill',
+                ),
+                ActivityCard(
+                  date: '06/04/2021',
+                  activity: 'DEPOSIT CREDIT TO 5684714XXX  ',
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
