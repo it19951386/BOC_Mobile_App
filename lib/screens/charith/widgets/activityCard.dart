c41ae165660887924db6f182d3ede039a63bb5d3
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:55:47 +0530

    add card content in activity Widget

--- lib/screens/charith/widgets/activityCard.dart
+++ lib/screens/charith/widgets/activityCard.dart
@@ -10,6 +10,31 @@ class ActivityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                this.date,
+                style: ApptextStyle.CARD_DATE,
+              ),
+              SizedBox(height: 10),
+              Text(
+                this.activity,
+                style: ApptextStyle.ACTIVITY,
+              ),
+            ],
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        elevation: 5.0,
+        margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+      ),
+    );
   }
 }
