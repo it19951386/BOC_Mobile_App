4b92f92078ad72daead93b5385d34e4a135c24dc
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:06:31 +0530

    add statful datepicker widget

---
+++ lib/screens/charith/widgets/datePicker.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
+
+class DatePicker extends StatefulWidget {
+  const DatePicker({Key? key}) : super(key: key);
+
+  @override
+  _DatePickerState createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
