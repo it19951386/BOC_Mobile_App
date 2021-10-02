4c746453a52ae48ae99074328db7f98f18b15544
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:13:35 +0530

    add one card and style

--- lib/screens/charith/widgets/reminder_card.dart
+++ lib/screens/charith/widgets/reminder_card.dart
@@ -7,6 +7,42 @@ class ReminderCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(12.0, 20.0, 12.0, 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Text(
+                    '12/06/2021',
+                    textAlign: TextAlign.right,
+                    style: ApptextStyle.CARD_DATE,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Transfer Credit',
+                  style: ApptextStyle.CARD_TITLE,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'This is card body',
+                  style: ApptextStyle.CARD_SUB_TITLE,
+                ),
+              ],
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          elevation: 5.0,
+          margin: EdgeInsets.fromLTRB(30, 40, 30, 0.0),
+        ),
+      ],
+    );
   }
 }
