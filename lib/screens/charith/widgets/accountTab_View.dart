17381b6ef723d75d1344c0f505856d66447542d4
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:57:35 +0530

    craete accountTabView.dart

---
+++ lib/screens/charith/widgets/accountTab_View.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
+
+class ActivityTab extends StatelessWidget {
+  const ActivityTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
