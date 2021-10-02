aa893607199addc29e2bee4bb00475f921762304
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:15:28 +0530

    add second and styles to reminderCadr.dart

--- lib/screens/charith/widgets/reminder_card.dart
+++ lib/screens/charith/widgets/reminder_card.dart
@@ -42,6 +42,39 @@ class ReminderCard extends StatelessWidget {
           elevation: 5.0,
           margin: EdgeInsets.fromLTRB(30, 40, 30, 0.0),
         ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(12.0, 20.0, 12.0, 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Text(
+                    '26/05/2021',
+                    textAlign: TextAlign.right,
+                    style: ApptextStyle.CARD_DATE,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Request Account Status',
+                  style: ApptextStyle.CARD_TITLE,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'This is card body',
+                  style: ApptextStyle.CARD_SUB_TITLE,
+                ),
+              ],
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          elevation: 5.0,
+          margin: EdgeInsets.fromLTRB(30, 20, 30, 0.0),
+        ),
       ],
     );
   }
