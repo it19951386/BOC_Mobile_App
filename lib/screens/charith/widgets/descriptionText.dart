dc023a06c7e963e435cf0812ac3abedcf8e960fd
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-02 23:59:38 +0530

    add const to constructor

--- lib/screens/charith/widgets/descriptionText.dart
+++ lib/screens/charith/widgets/descriptionText.dart
@@ -2,7 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
 
 class DescriptionText extends StatelessWidget {
-  const DescriptionText({Key? key}) : super(key: key);
+  const DescriptionText({
+    Key? key,
+    required this.icon,
+    required this.hint,
+    required this.label,
+    required this.inputType,
+    required this.inputAction,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String hint;
+  final String label;
+  final TextInputType inputType;
+  final TextInputAction inputAction;
 
   @override
   Widget build(BuildContext context) {
