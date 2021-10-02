c09ccf55073ac1bfad9b159b8cb6bb4ead578d3d
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:08:04 +0530

    create datepicker widget

--- lib/screens/charith/widgets/datePicker.dart
+++ lib/screens/charith/widgets/datePicker.dart
@@ -3,13 +3,37 @@ import 'package:intl/intl.dart';
 import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
 
 class DatePicker extends StatefulWidget {
-  const DatePicker({Key? key}) : super(key: key);
+  const DatePicker({
+    Key? key,
+    required this.icon,
+    required this.hint,
+    required this.label,
+  }) : super(key: key);
 
   @override
   _DatePickerState createState() => _DatePickerState();
+
+  final IconData icon;
+  final String hint;
+  final String label;
 }
 
 class _DatePickerState extends State<DatePicker> {
+  TextEditingController dateinput = TextEditingController();
+
+  //getting variable access
+  get hint => hint;
+  get label => label;
+  IconData? get icon => icon;
+
+  //text editing controller for text field
+
+  @override
+  void initState() {
+    dateinput.text = ""; //set the initial value of text field
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
