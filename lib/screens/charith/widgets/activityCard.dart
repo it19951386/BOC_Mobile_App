d4e5c86a81099fbaaed408088394dc6971c208b1
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:54:53 +0530

    create activitycard widget

---
+++ lib/screens/charith/widgets/activityCard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
+
+class ActivityCard extends StatelessWidget {
+  const ActivityCard({Key? key, required this.date, required this.activity})
+      : super(key: key);
+
+  final String date;
+  final String activity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
