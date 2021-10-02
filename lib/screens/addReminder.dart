4f8e15b42b7d6f486b2b07719ffb1b7432b5bbce
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:20:42 +0530

    import customApp bar from pasindu's screen

--- lib/screens/addReminder.dart
+++ lib/screens/addReminder.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
+import 'package:boc_mobile_app/screens/pasindu/customAppBar.dart';
 
 class AddReminder extends StatefulWidget {
   const AddReminder({Key? key}) : super(key: key);
