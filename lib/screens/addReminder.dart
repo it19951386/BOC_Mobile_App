0870864f8f2b4c0299fd2d960e77060b7b4a6645
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:19:47 +0530

    create addReminder widget

---
+++ lib/screens/addReminder.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
+
+class AddReminder extends StatefulWidget {
+  const AddReminder({Key? key}) : super(key: key);
+
+  @override
+  _AddReminderState createState() => _AddReminderState();
+}
+
+class _AddReminderState extends State<AddReminder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
