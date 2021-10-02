fa67baf8a5bccba77c6c3a0efda78cef49ed5c53
IT19986722-Charith <it19986722@my.sliit.lk>
2021-10-03 00:02:14 +0530

    add clear button styles

--- lib/screens/charith/widgets/Clear_Button.dart
+++ lib/screens/charith/widgets/Clear_Button.dart
@@ -5,6 +5,37 @@ class ClearButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 3.0, color: Colors.amberAccent),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 5, color: Colors.grey, offset: Offset(0, 4))
+              ]),
+          child: FlatButton(
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                child: Text(
+                  'Clear',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins-Medium',
+                  ),
+                ),
+              ),
+              color: Colors.white,
+              shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(20.0),
+              )),
+        ),
+      ],
+    );
   }
 }
