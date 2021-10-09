14e5eb6d9da75488037f49781a265787707ee873
D.I.P Lakshan <it19951836@my.sliit.lk>
2021-10-09 18:22:22 +0530

    add side bar

---
+++ lib/screens/ishanka/widgets/SideBar/navigation_drawer_widget.dart
@@ -0,0 +1,172 @@
+import 'package:boc_mobile_app/screens/ishanka/Profile.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class NavigationDrawerWidget extends StatelessWidget {
+  final padding = EdgeInsets.symmetric(horizontal: 20);
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Welcome To';
+    final title2 = 'Boc Bank App';
+    final urlImage =
+        'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/102012/boc.jpg?itok=STSg97D3';
+
+    return Drawer(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 253, 197, 12),
+              Color.fromARGB(164, 253, 197, 12)
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.bottomRight,
+            stops: [0.0, 0.8],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: ListView(
+          children: <Widget>[
+            buildHeader(
+              urlImage: urlImage,
+              title: title,
+              title2: title2,
+              onClicked: () {},
+
+            ),
+            Container(
+              padding: padding,
+              child: Column(
+                children: [
+                  const SizedBox(height: 12),
+                  buildSearchField(),
+                  const SizedBox(height: 24),
+                  buildMenuItem(
+                    text: 'Profile',
+                    icon:  FontAwesomeIcons.user,
+                    onClicked: () => selectedItem(context, 0),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'View Utility Bill',
+                    icon: Icons.web_rounded,
+                    onClicked: () => selectedItem(context, 1),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'Add Utility Bill',
+                    icon: Icons.add_chart,
+                    onClicked: () => selectedItem(context, 2),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'Calculator',
+                    icon: Icons.calculate_outlined,
+                    onClicked: () => selectedItem(context, 3),
+                  ),
+                  const SizedBox(height: 24),
+                  const SizedBox(height: 24),
+
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildHeader({
+    required String urlImage,
+    required String title,
+    required String title2,
+    required VoidCallback onClicked,
+  }) =>
+      InkWell(
+        onTap: onClicked,
+        child: Container(
+          padding: padding.add(EdgeInsets.symmetric(vertical: 40)),
+          child: Row(
+            children: [
+              CircleAvatar(radius: 30, backgroundImage: NetworkImage(urlImage)),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    title2,
+                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  ),
+                ],
+              ),
+              Spacer(),
+
+            ],
+          ),
+        ),
+      );
+
+  Widget buildSearchField() {
+    final color = Colors.black45;
+
+    return TextField(
+      style: TextStyle(color: color),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        hintText: 'Search',
+        hintStyle: TextStyle(color: color),
+        prefixIcon: Icon(Icons.search, color: color),
+        filled: true,
+        fillColor: Colors.white12,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: color.withOpacity(0.7)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: color.withOpacity(0.7)),
+        ),
+      ),
+    );
+  }
+
+  Widget buildMenuItem({
+    required String text,
+    required IconData icon,
+    VoidCallback? onClicked,
+  }) {
+    final color = Colors.black;
+    final hoverColor = Colors.black;
+
+    return ListTile(
+      leading: Icon(icon, color: color),
+      title: Text(text, style: TextStyle(color: color,fontFamily: 'Poppins',)),
+      hoverColor: hoverColor,
+      onTap: onClicked,
+    );
+  }
+
+  void selectedItem(BuildContext context, int index) {
+
+
+    switch (index) {
+      case 0:
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => Profile(),
+        ));
+        break;
+      // case 1:
+      //   Navigator.of(context).push(MaterialPageRoute(
+      //     builder: (context) => FavouritesPage(),
+      //   ));
+      //   break;
+    }
+  }
+}
