d73e90683f3f90b09c705e00c6f076ccbfd7759c
Charith <wcharithbuddhipriya@gmail.com>
2021-10-10 02:35:53 +0530

    add time textwidget to addreminder form

--- lib/screens/charith/AddReminderForm.dart
+++ lib/screens/charith/AddReminderForm.dart
@@ -217,6 +217,83 @@ class _AddReminderFormState extends State<AddReminderForm> {
                     },
                   ),
                 ),
+                SizedBox(height: 10),
+                /****************************
+                    Time Picker Container
+                 ****************************/
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 3.0, color: Colors.amberAccent),
+                      borderRadius: BorderRadius.circular(14),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 5,
+                            color: Colors.grey,
+                            offset: Offset(0, 4))
+                      ]),
+                  child: Center(
+                    child: TextFormField(
+                      controller:
+                          _timeController, //editing controller of this TextField
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 10),
+                        border: InputBorder.none,
+                        hintText: 'Time',
+                        filled: true,
+                        errorStyle: TextStyle(
+                            color: Colors.deepOrange,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Poppins'),
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Icon(
+                            Icons.timer,
+                            size: 18,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        hintStyle: ApptextStyle.HINT_TEXT,
+                      ),
+                      readOnly:
+                          true, //set it true, so that user will not able to edit text
+                      onTap: () async {
+                        TimeOfDay? pickedTime = await showTimePicker(
+                          initialTime: TimeOfDay.now(),
+                          context: context,
+                        );
+
+                        if (pickedTime != null) {
+                          print(pickedTime.format(context)); //output 10:51 PM
+                          DateTime parsedTime = DateFormat.jm()
+                              .parse(pickedTime.format(context).toString());
+                          //converting to DateTime so that we can further format on different pattern.
+                          print(parsedTime); //output 1970-01-01 22:53:00.000
+                          String formattedTime =
+                              DateFormat('HH:mm:ss').format(parsedTime);
+                          print(formattedTime); //output 14:59:00
+                          //DateFormat() is from intl package, you can format the time on any pattern you need.
+
+                          setState(() {
+                            _timeController.text =
+                                formattedTime; //set the value of text field.
+                          });
+                        } else {
+                          print("Time is not selected");
+                        }
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "     Please set a time";
+                        }
+                        getTime = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
