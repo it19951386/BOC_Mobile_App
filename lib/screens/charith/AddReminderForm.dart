101678b080cd7eb8d1f2c63e5aebcaa4b4cb6649
Charith <wcharithbuddhipriya@gmail.com>
2021-10-10 02:22:10 +0530

    add FocusNodes to reminderform

--- lib/screens/charith/AddReminderForm.dart
+++ lib/screens/charith/AddReminderForm.dart
@@ -5,6 +5,17 @@ import 'package:boc_mobile_app/screens/charith/widgets/export_widegts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AddReminderForm extends StatefulWidget {
+  final FocusNode subjectFocusNode;
+  final FocusNode descriptionFocusNode;
+  final FocusNode dateFocusNode;
+  final FocusNode timeFocusNode;
+
+  const AddReminderForm(
+      {required this.subjectFocusNode,
+      required this.descriptionFocusNode,
+      required this.dateFocusNode,
+      required this.timeFocusNode});
+
   @override
   _AddReminderFormState createState() => _AddReminderFormState();
 }
