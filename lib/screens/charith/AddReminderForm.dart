f78cb96140b6d2a77fc78035cc74554815e67f45
Charith <wcharithbuddhipriya@gmail.com>
2021-10-10 02:16:03 +0530

    create addReminderForm

---
+++ lib/screens/charith/AddReminderForm.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddReminderForm extends StatefulWidget {
+  const AddReminderForm({Key? key}) : super(key: key);
+
+  @override
+  _AddReminderFormState createState() => _AddReminderFormState();
+}
+
+class _AddReminderFormState extends State<AddReminderForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
